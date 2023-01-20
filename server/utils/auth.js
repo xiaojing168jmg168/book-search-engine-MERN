590f70e40733a7b6f23bc8643f8d6b3dadb41093
Xiaojing Deng <8redfishcreative@gmail.com>
2023-01-20 10:10:33 -0500

    update auth.js

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -6,9 +6,9 @@ const expiration = '2h';
 
 module.exports = {
   // function for our authenticated routes
-  authMiddleware: function (req, res, next) {
+  authMiddleware: function ({req}) {
     // allows token to be sent via  req.query or headers
-    let token = req.query.token || req.headers.authorization;
+    let token = req.query.token || req.query.token || req.headers.authorization;
 
     // ["Bearer", "<tokenvalue>"]
     if (req.headers.authorization) {
@@ -16,20 +16,21 @@ module.exports = {
     }
 
     if (!token) {
-      return res.status(400).json({ message: 'You have no token!' });
+      return req;
     }
 
     // verify token and get user data out of it
     try {
       const { data } = jwt.verify(token, secret, { maxAge: expiration });
       req.user = data;
-    } catch {
+    } catch(err) {
+      console.log(err);
       console.log('Invalid token');
-      return res.status(400).json({ message: 'invalid token!' });
+
     }
 
     // send to next endpoint
-    next();
+   return req;
   },
   signToken: function ({ username, email, _id }) {
     const payload = { username, email, _id };
