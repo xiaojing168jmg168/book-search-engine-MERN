1d3ae7cb29845f3925f7473c078948a793c8e26a
Xiaojing Deng <8redfishcreative@gmail.com>
2023-01-23 09:19:42 -0500

    update client pages

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react';
 import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from 'react-bootstrap';
 
 import Auth from '../utils/auth';
+import { useMutation } from '@apollo/client';
+import { SAVE_BOOK } from '../utils/mutations';
 import { saveBook, searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
 
@@ -13,6 +15,7 @@ const SearchBooks = () => {
 
   // create state to hold saved bookId values
   const [savedBookIds, setSavedBookIds] = useState(getSavedBookIds());
+  const [ saveBook ] = useMutation(SAVE_BOOK);
 
   // set up useEffect hook to save `savedBookIds` list to localStorage on component unmount
   // learn more here: https://reactjs.org/docs/hooks-effect.html#effects-with-cleanup
@@ -57,27 +60,14 @@ const SearchBooks = () => {
     // find the book in `searchedBooks` state by the matching id
     const bookToSave = searchedBooks.find((book) => book.bookId === bookId);
 
-    // get token
-    const token = Auth.loggedIn() ? Auth.getToken() : null;
-
-    if (!token) {
-      return false;
-    }
-
-    try {
-      const response = await saveBook(bookToSave, token);
-
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-
-      // if book successfully saves to user's account, save book id to state
-      setSavedBookIds([...savedBookIds, bookToSave.bookId]);
-    } catch (err) {
+    try{
+      await saveBook({ variables: { input: bookToSave }});
+      //if book successfully saves to user's account, save book id to state
+      setSavedBookIds([...saveBookIds, bookToSave.bookId]);
+    }catch(err){
       console.error(err);
     }
-  };
-
+  }
   return (
     <>
       <Jumbotron fluid className='text-light bg-dark'>
